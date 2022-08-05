39f8686de3f9b477cc6c669ae5f9a212646652a6
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-08-05 11:13:40 -0700

    auto push

--- src/main/java/com/johncarter/InboxApp.java
+++ src/main/java/com/johncarter/InboxApp.java
@@ -2,10 +2,10 @@ package com.johncarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -14,12 +14,15 @@ public class InboxApp {
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApp.class, args);
 	}
-
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
-	}
 	
+    /**
+    * connnects the Spring Boot application to the Astra secure bundle
+    */
+
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 
 }
